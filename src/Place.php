3d54c6c01e0cbfe9da2ff5128ca14af8dbd6472b
Brian Borealis & Julian Stewart <student@epicodus.com>
2015-08-05 16:30:07 -0700

    added app.php, Place.php, index.php, and html.twig files

---
+++ src/Place.php
@@ -0,0 +1,37 @@
+<?php
+class Place
+{
+    private $city;
+
+    function __construct($city)
+    {
+        $this->city = $city;
+    }
+
+    function setCity($new_city)
+    {
+        $this->city = (string) $new_city;
+    }
+
+    function getCity()
+    {
+        return $this->city;
+    }
+
+    function save()
+    {
+        array_push($_SESSION['list_of_places'], $this);
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['list_of_places'];
+    }
+
+    static function deleteAll()
+    {
+        $_SESSION['list_of_places'] = array();
+    }
+}
+
+?>
